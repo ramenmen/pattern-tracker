3fcb5489c30d733bb901b2e279b73a7e976b2be2
malaimo <chuyue.liu@gmail.com>
2023-03-20 18:12:58 +0800

    Remove unused files

--- notes.js
+++
@@ -1,9 +0,0 @@
-//temp: set up pattern
-// const myPattern = {};
-// myPattern.name = 'mynameee';
-// myPattern.rows = [];
-// for (i=1;i<=5;i++) {
-//     myPattern.rows[i-1] = {"desc":`This is row ${i}.bla bla bla bla bla bla bla bla bla bla`};
-// }
-// let currentPattern = JSON.stringify(myPattern);
-
